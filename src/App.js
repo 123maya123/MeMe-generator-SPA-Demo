348fdc60ffab6e42676fb6b0add6b890fe6df9c6
123maya123 <manjulacanada33@gmail.com>
2021-12-06 14:10:44 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './style.css'
+import MemeGenerator from './MemeGenerator';
+import Header from './Header'
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <MemeGenerator/>
+   </div>
+  );
+}
+
+export default App;
